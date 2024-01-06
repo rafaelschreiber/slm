93678f12a37b32723a9217cf092e490f40477219
unknown <njmeem95@gmail.com>
2024-01-06 10:43:48 +0100

    Implemented Unit Test for hasWinner Method of Class TicTacToe

--- src/test/java/org/cybercitadel/TicTacToeTest.java
+++ src/test/java/org/cybercitadel/TicTacToeTest.java
@@ -13,4 +13,27 @@ public class TicTacToeTest {
         game.switchCurrentPlayer();
         assertEquals('X', game.getCurrentPlayer().getMarker());
     }
+    public void Test_hasWinner() {
+        TicTacToe game = new TicTacToe();
+        game.getBoard().place(0, 0, 'X');
+        game.getBoard().place(1, 1, 'X');
+        game.getBoard().place(2, 2, 'X');
+        assertEquals('X', game.hasWinner());
+        game.getBoard().clear();
+        game.getBoard().place(1, 0, 'o');
+        game.getBoard().place(1, 1, 'O');
+        game.getBoard().place(1, 2, 'o');
+        assertEquals('O', game.hasWinner());
+        game.getBoard().clear();
+        game.getBoard().place(0, 0, 'x');
+        game.getBoard().place(0, 1, 'o');
+        game.getBoard().place(0, 2, 'x');
+        game.getBoard().place(1, 0, 'x');
+        game.getBoard().place(1, 1, 'o');
+        game.getBoard().place(1, 2, 'x');
+        game.getBoard().place(2, 0, 'o');
+        game.getBoard().place(2, 1, 'x');
+        game.getBoard().place(2, 2, 'o');
+        assertEquals('D', game.hasWinner());
+    }
 }
