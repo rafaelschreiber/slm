9b3a4fdf305bbca10c7b7557fe8a1e0d3f6d46f2
unknown <njmeem95@gmail.com>
2024-01-06 10:40:37 +0100

    Implemented hasWinner Method to check if game has a winner

--- src/main/java/org/cybercitadel/TicTacToe.java
+++ src/main/java/org/cybercitadel/TicTacToe.java
@@ -63,7 +63,23 @@ public class TicTacToe {
                 board.place(row, col, currentPlayer.getMarker());
                 break;
             }
-            switchCurrentPlayer();
+            switch (hasWinner()) {
+                case 'X':
+                    System.out.println("Player X has won!");
+                    isGameActive = false;
+                    break;
+                case 'O':
+                    System.out.println("Player O has won!");
+                    isGameActive = false;
+                    break;
+                case 'D':
+                    System.out.println("Draw!");
+                    isGameActive = false;
+                    break;
+                default:
+                    switchCurrentPlayer();
+                    break;
+            }
         }
     }
 
@@ -78,4 +94,33 @@ public class TicTacToe {
     protected Player getCurrentPlayer() {
         return currentPlayer;
     }
+    public char hasWinner() {
+        // check for all possible win combinations
+        for (int a = 0; a < 8; a++) {
+            String line = switch (a) {
+                case 0 -> board.getCell(0, 0) + board.getCell(0, 1) + board.getCell(0, 2);
+                case 1 -> board.getCell(1, 0) + board.getCell(1, 1) + board.getCell(1, 2);
+                case 2 -> board.getCell(2, 0) + board.getCell(2, 1) + board.getCell(2, 2);
+                case 3 -> board.getCell(0, 0) + board.getCell(1, 0) + board.getCell(2, 0);
+                case 4 -> board.getCell(0, 1) + board.getCell(1, 1) + board.getCell(2, 1);
+                case 5 -> board.getCell(0, 2) + board.getCell(1, 2) + board.getCell(2, 2);
+                case 6 -> board.getCell(0, 0) + board.getCell(1, 1) + board.getCell(2, 2);
+                case 7 -> board.getCell(0, 2) + board.getCell(1, 1) + board.getCell(2, 0);
+                default -> null;
+            };
+
+            if (line.equals("XXX")) {
+                return 'X'; // return if winner is X
+            } else if (line.equals("OOO")) {
+                return 'O'; // return if winner is O
+            }
+        }
+
+        if (board.isFull()) {
+            return 'D'; // return if draw
+        } else {
+            return 'N'; // return if winner not decided
+        }
+    }
 }
+
